da4de9f8c307f5a7ee9cedd03bd6e1ecc5c71c66
Henriquedearaujo <ricke_cris@hotmail.com>
2024-08-05 15:05:24 -0300

    comfigurando e aplicando redux toolkit query

--- src/App.tsx
+++ src/App.tsx
@@ -14,21 +14,21 @@ export type Produto = {
 }
 
 function App() {
-  const [produtos, setProdutos] = useState<Produto[]>([])
-  const [favoritos] = useState<Produto[]>([])
+  // const [produtos, setProdutos] = useState<Produto[]>([])
+  // const [favoritos] = useState<Produto[]>([])
 
-  useEffect(() => {
-    fetch('https://fake-api-tau.vercel.app/api/ebac_sports')
-      .then((res) => res.json())
-      .then((res) => setProdutos(res))
-  }, [])
+  // useEffect(() => {
+  //   fetch('ebac_sports')
+  //     .then((res) => res.json())
+  //     .then((res) => setProdutos(res))
+  // }, [])
 
   return (
     <Provider store={store}>
       <GlobalStyle />
       <div className="container">
         <Header />
-        <Produtos produtos={produtos} favoritos={favoritos} />
+        <Produtos favoritos={[]} />
       </div>
     </Provider>
   )
